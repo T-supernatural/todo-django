22638f276d93d2ce18f3fa13ae63d47a503d99d4
T-supernatural <tinuademichael@gmail.com>
2025-07-16 19:35:49 +0100

    Added tailwind

--- manage.py
+++ manage.py
@@ -15,6 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # Check if runserver is being called and set default port
+    if len(sys.argv) > 1 and sys.argv[1] == 'runserver' and len(sys.argv) == 2:
+        sys.argv.append('1000')  # Replace 8080 with your desired port
     execute_from_command_line(sys.argv)
 
 
